3a1777d8af60cb6a8a887890c4e2a879d884f1f1
jorenca <mailnamefrustration@gmail.com>
2020-02-22 17:05:41 +0200

    Create React web app for hub UI

---
+++ web-ui/build/precache-manifest.d07294347950fcfaccb110a887457e90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50d733f3e3297c49a2736fcc4219d345",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0905e5a6139152189cd",
+    "url": "/static/css/main.7a680cc3.chunk.css"
+  },
+  {
+    "revision": "24b94ad5069b19b91c95",
+    "url": "/static/js/2.1394080e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.1394080e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0905e5a6139152189cd",
+    "url": "/static/js/main.bb302f1a.chunk.js"
+  },
+  {
+    "revision": "231088bf6667e65beea8",
+    "url": "/static/js/runtime-main.5b360307.js"
+  }
+]);
